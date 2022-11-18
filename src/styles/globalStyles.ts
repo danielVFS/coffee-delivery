8273a2cdd8a2fa8fd5140d5853f84691055cd28e
danielVFS <danielv759153@gmail.com>
2022-11-18 20:26:42 -0300

    styles: global styles

---
+++ src/styles/globalStyles.ts
@@ -0,0 +1,21 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+  * {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box
+  }
+
+  body {
+    -webkit-font-smoothing: antialiased;
+    color: ${(props) => props.theme["base-text"]};
+    background: ${(props) => props.theme.background}
+  }
+
+  body, input, textarea, button {
+    font-family: 'Roboto', sans-serif;
+    font-weight: 400;
+    font-size: 1rem;
+  }
+`;
