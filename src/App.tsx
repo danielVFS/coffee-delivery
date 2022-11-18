2c147d2afacd812527ea81b816efc2595a4348d3
danielVFS <danielv759153@gmail.com>
2022-11-18 19:19:43 -0300

    feat: styled components provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import { ThemeProvider } from "styled-components";
+import { defaultTheme } from "./styles/themes/default";
+
 function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyles />
+      <Router />
+    </ThemeProvider>
+  );
 }
 
 export default App;
