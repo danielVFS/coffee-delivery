0b8fb2fdb15703e730dac7aa8ac764b653e6a064
danielVFS <danielv759153@gmail.com>
2022-11-19 15:23:38 -0300

    fix(Texy): extends html attributes

--- src/components/Text/index.tsx
+++ src/components/Text/index.tsx
@@ -1,14 +1,14 @@
 import { TextContainer, TEXT_SIZES } from "./styles";
 
-interface TextProps extends React.HTMLProps<HTMLParagraphElement> {
+interface TextProps extends React.HTMLAttributes<HTMLParagraphElement> {
   variant: keyof typeof TEXT_SIZES;
   bold?: boolean;
   children: string;
 }
 
-export function Text({ variant, bold, children }: TextProps) {
+export function Text({ variant, bold, children, ...props }: TextProps) {
   return (
-    <TextContainer variant={variant} bold={bold}>
+    <TextContainer variant={variant} bold={bold} {...props}>
       {children}
     </TextContainer>
   );
