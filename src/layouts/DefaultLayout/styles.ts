c0c8a757c02a29d49b045167b430f59cb17af5a5
danielVFS <danielv759153@gmail.com>
2022-11-26 15:42:35 -0300

    style(DefaultLayout): decrease max-width

--- src/layouts/DefaultLayout/styles.ts
+++ src/layouts/DefaultLayout/styles.ts
@@ -1,9 +1,8 @@
 import styled from "styled-components";
 
 export const LayoutContainer = styled.div`
-  max-width: 90rem;
+  max-width: 70rem;
   margin: 0 auto;
-  padding: 0 2rem;
 
   background: ${(props) => props.theme.background};
 
