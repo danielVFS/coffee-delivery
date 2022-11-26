695aae79f961d4111f01e5db86f5cda8d11bb1fa
danielVFS <danielv759153@gmail.com>
2022-11-26 14:55:42 -0300

    feat(Header): count coffess added to cart

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,19 @@
 import { MapPin, ShoppingCart } from "phosphor-react";
+import { useContext } from "react";
 import Logo from "../../assets/Logo.svg";
+import { CoffeeContext } from "../../contexts/CoffeesContext";
 import { IconButton } from "../IconButton";
 import { Text } from "../Text";
 import { HeaderActions, HeaderContainer, LocationContainer } from "./styles";
 
 export function Header() {
+  const { coffees } = useContext(CoffeeContext);
+
+  const coffeeSum = coffees.reduce(
+    (acc, current) => acc + (current.amount ? current.amount : 0),
+    0
+  );
+
   return (
     <HeaderContainer>
       <img src={Logo} alt="Coffee Delivery logo" />
@@ -16,9 +25,15 @@ export function Header() {
           </Text>
         </LocationContainer>
 
-        <IconButton variant="yellow">
-          <ShoppingCart weight="fill" />
-        </IconButton>
+        {coffeeSum === 0 ? (
+          <IconButton variant="yellow">
+            <ShoppingCart weight="fill" />
+          </IconButton>
+        ) : (
+          <IconButton variant="yellow" count={coffeeSum}>
+            <ShoppingCart weight="fill" />
+          </IconButton>
+        )}
       </HeaderActions>
     </HeaderContainer>
   );
